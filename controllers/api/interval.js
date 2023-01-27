8a548f70ea71e3d94f40b621729ecbffc4b090fa
Aidan <aidanreynolds715@gmail.com>
2023-01-26 21:29:46 -0500

    additions for alpha vantage

--- controllers/api/interval.js
+++ controllers/api/interval.js
@@ -1,18 +1,18 @@
-const DAILY = 'TIME_SERIES_DAILY_ADJUSTED';
-const WEEKLY = 'TIME_SERIES_WEEKLY_ADJUSTED';
-const MONTHLY = 'TIME_SERIES_MONTHLY_ADJUSTED';
+// const DAILY = 'TIME_SERIES_DAILY_ADJUSTED';
+// const WEEKLY = 'TIME_SERIES_WEEKLY_ADJUSTED';
+// const MONTHLY = 'TIME_SERIES_MONTHLY_ADJUSTED';
 
-const interval = (type) => {
-    switch (type) {
-        case 'daily':
-        return DAILY;
-        case 'weekly':
-        return WEEKLY;
-        case 'monthly':
-        return MONTHLY;
-        default:
-        return DAILY;
-    }
-};
+// const interval = (type) => {
+//     switch (type) {
+//         case 'daily':
+//         return DAILY;
+//         case 'weekly':
+//         return WEEKLY;
+//         case 'monthly':
+//         return MONTHLY;
+//         default:
+//         return DAILY;
+//     }
+// };
 
-module.exports = interval;
+// module.exports = interval;
--- controllers/api/stock.js
+++ controllers/api/stock.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const app = express();
 
-// const alpha = require('alphavantage')({ key: 'GPC9UGIE9X6VAHJ0' });  // npm alphavantage for npm install (may not use)
+const react = require('react');
+const CanvasJSReact = require('./canvasjs.react.js');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+// key: 'GPC9UGIE9X6VAHJ0' 
 
 const cors = require('cors');
 router.use(cors());
@@ -16,6 +21,10 @@ dotenv.config();
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+// app.get('/', function (req, res) {
+//     res.render('list',{query, stock_Dates, stock_Open, stock_High, stock_Low, stock_Close, stock_Volume});
+// });
+
 //const interval = require('/interval.js'); // not sure if this is needed
 
 // ***** Code for specific intervals if we implement a desired interval *****
@@ -26,42 +35,87 @@ router.use(bodyParser.json());
 
 //     const url = `https://www.alphavantage.co/query?function=${interval}&symbol=${symbol}&interval=${range}&apikey=${process.env.API_KEY}`;
 
-//     try {
-//         axios.get(url)
-//             .then((response) => {
-//             res.json(response.data);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     const output = getChartData(apiData.data, req.params.type);
-//     res.json(output);
-//     } catch (err){
-//     console.log(err)
-//     res.status(500).json(err);
-
-//     }
 // });
 
 // Daily data for specific stock symbol
 router.get('/daily/:symbol', function (req, res) {
     const symbol = req.params.symbol;
     
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&interval=30min&apikey=${process.env.API_KEY}`;
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=30min&apikey=${process.env.API_KEY}`;
 
-    try {
-        axios.get(url)
-            .then((response) => {
-            res.json(response.data);
+    class App extends Component {
+        constructor() {
+            super();
+            this.state = {
+                dataPoint: []
+            }
+        }
+    componentChart() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const stockData = data['Time Series (IntraDaily)'];
+            const newData = Object.keys(stockData).reduce((result, key) => {
+                const value = stockData[key];
+                const tmp = {
+                    x: new Date(key),
+                    y: [parseFloat(value['1. open']), parseFloat(value['2. high']), 
+                    parseFloat(value['3. low']), parseFloat(value['4. close'], parseFloat(value['5. volume']))],
+                    color: parseFloat(value['4. close']) > parseFloat(value['1. open']) ? 'green' : 'red'
+                }
+                result.push(tmp);
+                return result;
+            }, [])
+            console.log(newData);
+            this.setState({
+                dataPoint: newData.slice(0, 50)
+            })
         })
-        .catch((err) => {
-            console.log(err);
-        });
-    } catch (err){
-    console.log(err)
-    res.status(500).json(err);
+    }
+    render () {
+        const options = {
+            title: {
+                text: "Stock Chart with Range Selector"
+            },
+            axisX: {
+                valueFormatString: "DD MMM"
+            },
+            axisY: {
+                prefix: "$",
+                title: "Price (in USD)"
+            },
+            toolTip: {
+                shared: true
+            },
+            legend: {
+                cursor: "pointer",
+                verticalAlign: "bottom",
+                horizontalAlign: "left",
+                dockInsidePlotArea: true,
+                itemclick: toogleDataSeries
+            },
+            data: [{
+                type: "candlestick",
+                name: "${symbol}",
+                showInLegend: true,
+                yValueFormatString: "$#,###.##",
+                xValueFormatString: "DD MMM, YYYY",
+                dataPoints: [
+                    this.state.data_points
+                ]
+            }]
+        }
+        return (
+            <div>
+                <CanvasJSChart options = {options}
+                    onRef={ref => this.chart = ref}
+                />
+            </div>
+        );
+    }
     }
 });
+
 
 //weekly data for specific stock symbol
 router.get('/weekly/:symbol', function (req, res) {
@@ -69,19 +123,77 @@ router.get('/weekly/:symbol', function (req, res) {
 
     const url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=${symbol}&apikey=${process.env.API_KEY}`;
     
-    try {
-        axios.get(url)
-            .then((response) => {
-            res.json(response.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    } catch (err){
-    console.log(err)
-    res.status(500).json(err);
+    class App extends Component {
+        constructor() {
+            super();
+            this.state = {
+                dataPoint: []
+            }
+        }
+    componentChart() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const stockData = data['Weekly Time Series'];
+        const newData = Object.keys(stockData).reduce((result, key) => {
+            const value = stockData[key];
+            const tmp = {
+                x: new Date(key),
+                y: [parseFloat(value['1. open']), parseFloat(value['2. high']), 
+                parseFloat(value['3. low']), parseFloat(value['4. close'], parseFloat(value['5. volume']))],
+                color: parseFloat(value['4. close']) > parseFloat(value['1. open']) ? 'green' : 'red'
+            }
+            result.push(tmp);
+            return result;
+        }, [])
+        console.log(newData);
+        res.json(newData);
+    })
+    }
+    render() {
+    const options = {
+        title: {
+            text: "Stock Chart with Range Selector"
+        },
+        axisX: {
+            valueFormatString: "DD MMM"
+        },
+        axisY: {
+            prefix: "$",
+            title: "Price (in USD)"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            verticalAlign: "bottom",
+            horizontalAlign: "left",
+            dockInsidePlotArea: true,
+            itemclick: toogleDataSeries
+        },
+        data: [{
+            type: "candlestick",
+            name: "${symbol}",
+            showInLegend: true,
+            yValueFormatString: "$#,###.##",
+            xValueFormatString: "DD MMM, YYYY",
+            dataPoints: [
+                this.state.data_points
+            ]
+        }]
+    }
+    return (
+        <div>
+            <CanvasJSChart options = {options}
+                onRef={ref => this.chart = ref}
+            />
+        </div>
+    );
+    }
     }
 });
+
 
 //monthly data for specific stock symbol
 router.get('/monthly/:symbol', function (req, res) {
@@ -89,19 +201,81 @@ router.get('/monthly/:symbol', function (req, res) {
 
     const url = `https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${process.env.API_KEY}`;
 
-    try {
-        axios.get(url)
-            .then((response) => {
-            res.json(response.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    } catch (err){
-    console.log(err)
-    res.status(500).json(err);
+    class App extends Component {
+        constructor() {
+            super();
+            this.state = {
+                dataPoint: []
+            }
+        }
+    componentChart() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const stockData = data['Monthly Time Series'];
+        const newData = Object.keys(stockData).reduce((result, key) => {
+            const value = stockData[key];
+            const tmp = {
+                x: new Date(key),
+                y: [parseFloat(value['1. open']), parseFloat(value['2. high']), 
+                parseFloat(value['3. low']), parseFloat(value['4. close'], parseFloat(value['5. volume']))],
+                color: parseFloat(value['4. close']) > parseFloat(value['1. open']) ? 'green' : 'red'
+            }
+            result.push(tmp);
+            return result;
+        }, []);
+        console.log(stockData);
+        res.json(newData);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    }
+        render() {
+            const options = {
+                title: {
+                    text: "Stock Chart with Range Selector"
+                },
+                axisX: {
+                    valueFormatString: "DD MMM"
+                },
+                axisY: {
+                    prefix: "$",
+                    title: "Price (in USD)"
+                },
+                toolTip: {
+                    shared: true
+                },
+                legend: {
+                    cursor: "pointer",
+                    verticalAlign: "bottom",
+                    horizontalAlign: "left",
+                    dockInsidePlotArea: true,
+                    itemclick: toogleDataSeries
+                },
+                data: [{
+                    type: "candlestick",
+                    name: "${symbol}",
+                    showInLegend: true,
+                    yValueFormatString: "$#,###.##",
+                    xValueFormatString: "DD MMM, YYYY",
+                    dataPoints: [
+                        this.state.data_points
+                    ]
+                }]
+            }
+            return (
+                <div>
+                    <CanvasJSChart options = {options}
+                        onRef={ref => this.chart = ref}
+                    />
+                </div>
+            );
+        }
     }
 });
+
+
 
 //yearly data for specific stock symbol
 router.get('/yearly/:symbol', function (req, res) {
@@ -109,17 +283,77 @@ router.get('/yearly/:symbol', function (req, res) {
     
     const url = `https://www.alphavantage.co/query?function=TIME_SERIES_YEARLY&symbol=${symbol}&apikey=${process.env.API_KEY}`;
 
-    try {
-        axios.get(url)
-            .then((response) => {
-            res.json(response.data);
+    class App extends Component {
+        constructor() {
+            super();
+            this.state = {
+                dataPoint: []
+            }
+        }
+    componentChart() {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const stockData = data['Monthly Time Series'];
+            const newData = Object.keys(stockData).reduce((result, key) => {
+                const value = stockData[key];
+                const tmp = {
+                    x: new Date(key),
+                    y: [parseFloat(value['1. open']), parseFloat(value['2. high']), 
+                    parseFloat(value['3. low']), parseFloat(value['4. close'], parseFloat(value['5. volume']))],
+                    color: parseFloat(value['4. close']) > parseFloat(value['1. open']) ? 'green' : 'red'
+                }
+                result.push(tmp);
+                return result;
+            }, []);
+            console.log(stockData);
+            res.json(newData);
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
-        });
-    } catch (err){
-    console.log(err)
-    res.status(500).json(err);
+        })
+    }
+        render() {
+            const options = {
+                title: {
+                    text: "Stock Chart with Range Selector"
+                },
+                axisX: {
+                    valueFormatString: "DD MMM"
+                },
+                axisY: {
+                    prefix: "$",
+                    title: "Price (in USD)"
+                },
+                toolTip: {
+                    shared: true
+                },
+                legend: {
+                    cursor: "pointer",
+                    verticalAlign: "bottom",
+                    horizontalAlign: "left",
+                    dockInsidePlotArea: true,
+                    itemclick: toogleDataSeries
+                },
+                data: [{
+                    type: "candlestick",
+                    name: "${symbol}",
+                    showInLegend: true,
+                    yValueFormatString: "$#,###.##",
+                    xValueFormatString: "DD MMM, YYYY",
+                    dataPoints: [
+                        this.state.data_points
+                    ]
+                }]
+            }
+            return (
+                <div>
+                    <CanvasJSChart options = {options}
+                        onRef={ref => this.chart = ref}
+                    />
+                </div>
+            );
+        }
     }
 });
 
@@ -129,40 +363,16 @@ router.get('/yearly/:symbol', function (req, res) {
 
 //     const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${symbol}&apikey=${process.env.API_KEY}`;
     
-//     try {
-//         axios.get(url)
-//             .then((response) => {
-//             res.json(response.data);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         });
-//     } catch (err){
-        
-//     console.log(err)
-//     res.status(500).json(err);
-//     }
 // });
 
-//company overview from symbol
-router.get('/summary/:symbol/', function (req, res) {
-    const symbol = req.params.symbol;
+// //company overview from symbol
+// router.get('/summary/:symbol/', function (req, res) {
+//     const symbol = req.params.symbol;
 
-    const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${process.env.API_KEY}`;
+//     const url = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${process.env.API_KEY}`;
 
-    try {
-        axios.get(url)
-            .then((response) => {
-            res.json(response.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    } catch (err){
-    console.log(err)
-    res.status(500).json(err);
-    }
-});
+
+// });
 
 
 module.exports = router;
