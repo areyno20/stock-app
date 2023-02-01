ce872cde402e8aeb4ce8ada2831b89cee6d6f7ef
Nav Gill <nsgngng@gmail.com>
2023-01-31 20:24:55 -0500

    added currency files

--- controllers/currency/currency.js
+++ controllers/currency/currency.js
@@ -1,10 +1,15 @@
-async function dataLoad(currencyOne, currencyTwo) {
-    const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${currencyOne}&to_currency=${currencyTwo}&apikey=GPC9UGIE9X6VAHJ0`;
-    const response = await fetch(url); 
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
+//async function dataLoad(currencyOne, currencyTwo) {
+  //  const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${currencyOne}&to_currency=${currencyTwo}&apikey=GPC9UGIE9X6VAHJ0`;
+  //  const response = await fetch(url); 
+  //  const data = await response.json();
+  //  console.log(data);
+  //  return data;
+//}
+
+const select = document.querySelectorAll(".currency");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const ans = document.getElementById("ans");
 
 'use strict';
 var request = require('request');
@@ -26,7 +31,34 @@ request.get({
       console.log(data);
     }
 });
-const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
-const num = document.getElementById("num");
-const ans = document.getElementById("ans");
+function display(data) {
+    const entries = Object.entries(data);
+    for (var i = 0; i < entries.length; i++) {
+      select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+      select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+    }
+  }
+  
+  btn.addEventListener("click", () => {
+    let currency1 = select[0].value;
+    let currency2 = select[1].value;
+    let value = num.value;
+  
+    if (currency1 != currency2) {
+      convert(currency1, currency2, value);
+    } else {
+      alert("Choose Different Currencies !!!");
+    }
+  });
+  
+  function convert(currency1, currency2, value) {
+    const host = "api.frankfurter.app";
+    fetch(
+      `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
+    )
+      .then((val) => val.json())
+      .then((val) => {
+        console.log(Object.values(val.rates)[0]);
+        ans.value = Object.values(val.rates)[0];
+      });
+  }
