772a9d2737f4323db3ba19b9105c88c90c3e0f44
Nav Gill <nsgngng@gmail.com>
2023-01-31 20:00:51 -0500

    fixed links

--- controllers/currency/currency.js
+++ controllers/currency/currency.js
@@ -6,3 +6,27 @@ async function dataLoad(currencyOne, currencyTwo) {
     return data;
 }
 
+'use strict';
+var request = require('request');
+
+// replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+var url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=GPC9UGIE9X6VAHJ0';
+
+request.get({
+    url: url,
+    json: true,
+    headers: {'User-Agent': 'request'}
+  }, (err, res, data) => {
+    if (err) {
+      console.log('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Status:', res.statusCode);
+    } else {
+      // data is successfully parsed as a JSON object:
+      console.log(data);
+    }
+});
+const select = document.querySelectorAll(".currency");
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+const ans = document.getElementById("ans");
